0bbab6eff11f0264d2ae410bd7d9edecae5248f4
Michael Harris <mharris161@gmail.com>
2020-09-13 19:49:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BuildingEscape
+
+Developed with Unreal Engine 4 as part of an Unreal C++ course I am taking.
---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,35 @@
+// Copyright Michael Harris 2020
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	float OpenYaw = 90.0f;
+	
+	FRotator CurrRotation = GetOwner()->GetActorRotation();
+	CurrRotation.Yaw = OpenYaw;
+	GetOwner()->SetActorRotation(CurrRotation);
+}
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
